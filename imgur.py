785c3844bf052275567c9c1c65cc5796a0f542dc
Existanza <kraft.sc2@gmail.com>
2014-11-05 21:20:02 +0100

    Imgurbot

--- imgur.py
+++ imgur.py
@@ -6,6 +6,7 @@ import urllib.request
 import string
 import random
 import os
+import imghdr
 
 n, a, t = 0, 0, 0
 SIZE = 5             # length of the randomized image name; possible values: 5,7; default value: 5
@@ -32,13 +33,21 @@ def get_img(imgurl):
         try:
             n += 1
             t += len(raw_data)/(1024 ** 2)
+            ft = open('C:\\imgur' + str(SIZE) + '\\' + 'tmp' + '.gif','wb')
+            ft.write(raw_data)
+            ft.close()
+            ext = imghdr.what('C:\\imgur' + str(SIZE) + '\\' + 'tmp' + '.gif', raw_data)
+            if not ext:
+                ext = 'jpg'
+            os.remove('C:\\imgur' + str(SIZE) + '\\' + 'tmp' + '.gif')
+            fullurl = 'http://i.imgur.com/' + imgurl + '.' + ext
             print('\r', 'Try %d.' % a, 'Retrieved: {0}'.format(fullurl), ' saved %.2f MB, total %.2f MB' % (len(raw_data)/(1024 ** 2), t) )
-            f = open('C:\\imgur' + str(SIZE) + '\\' + imgurl + '.gif','wb')
+            f = open('C:\\imgur' + str(SIZE) + '\\' + imgurl + '.' + ext,'wb')
             f.write(raw_data)
             f.close()
         except:
             print('couldn\'t write to C:\\imgur' + str(SIZE) + '\\' + imgurl + '.gif')
-    print('\r' + str(a), end='')
+    print('\r' + 'Try ' + str(a) + '.', end='')
 
 if not os.path.exists('C:\\imgur' + str(SIZE) + '\\'):
     os.makedirs('C:\\imgur' + str(SIZE) + '\\')
