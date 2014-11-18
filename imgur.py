5e0039ea34f9ba8f42534e2530f84024df930984
Existanza <kraft.sc2@gmail.com>
2014-11-18 21:20:07 +0100

    Imgurbot 1.4

--- imgur.py
+++ imgur.py
@@ -2,20 +2,73 @@
 __author__ = 'Deca'
 # http://stackoverflow.com/questions/10945542/python3-urllib-image-retreval
 # http://www.wykop.pl/link/1008101/masz-za-duzo-wolnego-czasu-zapraszam/
+# http://pillow.readthedocs.org/en/latest/reference/Image.html
+# http://blog.zeevgilovitz.com/detecting-dominant-colours-in-python/
 import urllib.request
 import string
 import random
 import os
 import imghdr
+from PIL import Image
+
 
 n, a, t = 0, 0, 0
 SIZE = 5             # length of the randomized image name; possible values: 5,7; default value: 5
 DISK_SPACE_AVL = 1   # the maximum amount of disk space the program can use during its execution in GB; default value: 1
 path = os.path.join(os.path.expanduser('~'), 'Desktop', 'WAZNE-MATURA') + '\\'
 
+
 def random_img():
-    '''Randoms an url,'''
+    '''Randoms an url.'''
     return ''.join([random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for i in range(SIZE)])
+
+
+def open_image(tmp_path):
+    '''Finds the most common colour in the picture.'''
+    t_im = Image.open(tmp_path)
+    t_im.save(path + 'tmp_v2.gif', 'GIF')
+    t_im.close()
+
+    n_im = Image.open(path + 'tmp_v2.gif')
+    size_tuple = n_im.size
+    new_l = int(size_tuple[0]/4)
+    new_u = int(size_tuple[1]/2)
+    new_r = int(3*new_l)
+    new_b = int(size_tuple[1])
+    colors = [0 for x in range(256)]
+    i = new_l
+    j = new_u
+    while i < new_r:
+        while j < new_b:
+            xy_list = [i, j]
+            pixel = n_im.getpixel(tuple(xy_list))
+            colors[pixel] += 1
+            j += 1
+        i += 1
+        j = new_u
+    most_frequent_color = [0, 0]
+    for i in range(256):
+        if colors[i] > most_frequent_color[0]:
+            most_frequent_color[1] = i
+            most_frequent_color[0] = colors[i]
+    pixel = -1
+    while pixel != most_frequent_color[1]:
+        i = random.randint(new_l, new_r)
+        j = random.randint(new_u, new_b)
+        xy_list = [i, j]
+        pixel = n_im.getpixel(tuple(xy_list))
+    most_frequent_color_tuple = (i, j)
+    rgb_im = n_im.convert('RGB')
+    rgb_tuple = rgb_im.getpixel(most_frequent_color_tuple)
+    n_im.close()
+    print(rgb_tuple)
+    r, g, b = rgb_tuple
+    rgb_im.close()
+    if ((r-5) > g and (r-5) > b and r > 175) or (2*r > g and 2*r > 3*b):
+        return True
+    else:
+        return False
+
 
 def get_img(imgurl):
     '''Finds a random image and downloads it if the image exists.'''
@@ -34,24 +87,37 @@ def get_img(imgurl):
         try:
             n += 1
             t += len(raw_data)/(1024 ** 2)
-            ft = open(path + 'tmp' + '.gif','wb')
+            ft = open(tmp_path,'wb')
             ft.write(raw_data)
             ft.close()
-            ext = imghdr.what(path + 'tmp' + '.gif', raw_data)
+            ext = imghdr.what(tmp_path, raw_data)
+            is_nsfw = open_image(tmp_path)
             if not ext:
                 ext = 'jpg'
             os.remove(path + 'tmp' + '.gif')
+            os.remove(path + 'tmp_v2' + '.gif')
+
             fullurl = 'http://i.imgur.com/' + imgurl + '.' + ext
-            print('\r', 'Try %d.' % a, 'Retrieved: {0}'.format(fullurl), ' saved %.2f MB, total %.2f MB' % (len(raw_data)/(1024 ** 2), t) )
-            f = open(path + imgurl + '.' + ext,'wb')
+            print('\r', 'Try %d.' % a, 'Retrieved: {0}'.format(fullurl), ' saved %.2f MB, total %.2f MB' % (len(raw_data)/(1024 ** 2), t))
+            if is_nsfw:
+                save_path = path + 'nsfw' + '\\' + imgurl + '.' + ext
+            else:
+                save_path = path + imgurl + '.' + ext
+            f = open(save_path, 'wb')
             f.write(raw_data)
             f.close()
         except:
-            print('couldn\'t write to' + path + imgurl + '.gif')
+            print(' couldn\'t write to ' + path + imgurl + '.gif')
     print('\r' + 'Try ' + str(a) + '.', end='')
 
 if not os.path.exists(path):
     os.makedirs(path)
+if not os.path.exists(path + '\\' + 'nsfw' + '\\'):
+    os.makedirs(path + '\\' + 'nsfw' + '\\')
+
+tmp_path = path + 'tmp' + '.gif'
+
+random.seed()
 
 while t < DISK_SPACE_AVL*1024:
     get_img(random_img())
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from cx_Freeze import setup, Executable
 
 setup(
     name = 'Imgurbot',
-    version = "1.3",
+    version = "1.4",
     description = "Imgurbot",
     executables = [Executable("imgur.py")],
     )
